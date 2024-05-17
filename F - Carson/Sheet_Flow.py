d293d76c4a8024db7f7068a687837ba0cbc8b4f0
CarsonW503 <willcars@oregonstate.edu>
2024-05-17 10:06:56 -0700

    Update Sheet_Flow.py
    Accounting for the fact I get shiedds parameter from Maggie

--- F - Carson/Sheet_Flow.py
+++ F - Carson/Sheet_Flow.py
@@ -4,26 +4,6 @@ import numpy as np
 # Dohmen-Janssen 2001
 # Code by Carson Williams - Git: CarsonW503
 
-def shields(fw, ui, s, g, d50):
-    
-    '''
-    function for maximum shields parameter in oscillatory 
-    flow - dimensionless
-    
-    inputs:
-    fw: wave-current friction factor --> dimensionless
-    ui: crest trough velocity amplitude velocity
-    s: relative density
-    g: gravity acceleration
-    d50: median grain size [m]
-    
-    output:
-    shields parameter
-    '''
-    
-    shield = ((1/2)*fw*(ui**2)) / ((s-1)*g*d50)
-    return np.array(shield)
-
 def sfl_thickness(shield, d50):
     
     '''
@@ -31,14 +11,14 @@ def sfl_thickness(shield, d50):
     *dependant on grain size*
     
     inputs:
-    shield: shields parameter --> dimensionless
+    shield: shields parameter --> dimensionless (FROM MAGGIE EQ.17)
     d50: median grain size [m] (NEEDS TO BE IN METERS)
     
     output:
-    Sheet flow layer thickness [m]
+    Sheet flow layer thickness [m] as an array
     '''
     
-    d50 = d50*1000             #conversion from m to mm for calculation
+    d50 = d50*1000      #conversion from m to mm for calculation
     
     sfl = []
     if d50 <= 0.15:
