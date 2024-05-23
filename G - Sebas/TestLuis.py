ff7be10d5dc93c07d7ec5b309da6e688ba5d316a
luisdanielperez <perezsql@oregonstate.edu>
2024-05-23 00:37:47 -0700

    Testing

--- G - Sebas/TestLuis.py
+++ G - Sebas/TestLuis.py
@@ -49,6 +49,26 @@ s          = rho_s / rho     # Specific gravity of the sediment (considering qua
 
 #%%
 
+
+
+# Colin/Emily output:
+shields_aa, f_wc, f_wt, ksdelta, ksw, fdelta, f_w, eta = vda.modelfunctions.combined_wavefric_ripples(T_cu, T_c, T_tu, T_t, a_hat, u_hat,
+                                                                              u_delta, delta, d50, d90, s, g)
+# Maggie output:
+fwdelt_c, fwdelt_t, TwRe, theta_cmag, theta_tmag, theta_cx, theta_tx = vda.modelfunctions.currentfric(u_delta, u_hat, f_wc, f_wt, rho, f_w,
+                                                                                   c_w, u_crx, u_trx, s, g, d50, fdelta)
+
+# Carly output:
+dstar      = vda.modelfunctions.dimensionless_grainsize(d50)
+shields_cr = vda.modelfunctions.critical_shields(dstar)
+omega_c    = vda.modelfunctions.sandload_crest(theta_cx, shields_cr)
+omega_t    = vda.modelfunctions.sandload_trough(theta_tx, shields_cr)
+
+# Carson output:
+delta_sc = vda.modelfunctions.sfl_thickness(theta_cmag, d50)
+delta_st = vda.modelfunctions.sfl_thickness(theta_tmag, d50)
+
+# Luis output:
 def phaseLag(s, d50, eta, u_hat_c, u_hat_t, c_w, T_c, T_cu, delta_sc, T_t, T_tu, delta_st, omega_c, omega_t, alpha=8.2, xi=1.7):
     '''
     Inputs:
@@ -76,10 +96,10 @@ def phaseLag(s, d50, eta, u_hat_c, u_hat_t, c_w, T_c, T_cu, delta_sc, T_t, T_tu,
     omega_tc    -> sand load entrained during the wave trough period and transported during the crest period
     '''
 
-    # Calculate non-dimensional grain size
-    D_star = ((g * (s - 1) / (nu**2))**(1 / 3)) * d50; print(D_star)
+    # Non-dimensional grain size
+    D_star = ((g * (s - 1) / (nu**2))**(1 / 3)) * d50
 
-    # Calculate settling velocity
+    # Settling velocity [m/s] (following van Rijn (1984))
     if D_star**3 <= 16.187:
         w_s = nu * (D_star**3) / (18 * d50)
     elif 16.187 < D_star**3 <= 16187:
@@ -87,52 +107,55 @@ def phaseLag(s, d50, eta, u_hat_c, u_hat_t, c_w, T_c, T_cu, delta_sc, T_t, T_tu,
     elif D_star**3 > 16187:
         w_s = 1.1 * nu * (D_star**1.5) / d50
 
-    # Calculate phase lag parameters for the crest half cycle
-    if eta > 0:
+    # Phase lag parameters for the crest half cycle (P_c) and trough half cycle (P_t)
+    if eta > 0:    # Ripple regime
         P_c = alpha * ((1 - xi * u_hat_c) / c_w) * (eta / (2 * (T_c - T_cu) * w_s))
-    else:
+        P_t = alpha * ((1 + xi * u_hat_t) / c_w) * (eta / (2 * (T_t - T_tu) * w_s))
+        
+    elif eta == 0: # Sheet flow regime
         P_c = alpha * ((1 - xi * u_hat_c) / c_w) * (delta_sc / (2 * (T_c - T_cu) * w_s))
+        P_t = alpha * ((1 + xi * u_hat_t) / c_w) * (delta_st / (2 * (T_t - T_tu) * w_s))
 
-    # Calculate phase lag parameters for the trough half cycle
-    if eta > 0:
-        P_t = alpha * ((1 - xi * u_hat_t) / c_w) * (eta / (2 * (T_t - T_tu) * w_s))
-    else:
-        P_t = alpha * ((1 - xi * u_hat_t) / c_w) * (delta_st / (2 * (T_t - T_tu) * w_s))
-
-    # Calculate sand load entrainment during wave cycles
+    # Sand load entrainment during wave cycles
     if P_c <= 1:
         omega_cc = omega_c
         omega_ct = 0
-    else:
+    elif P_c > 1:
         omega_cc = omega_c / P_c
         omega_ct = (1 - 1 / P_c) * omega_c
 
     if P_t <= 1:
         omega_tt = omega_t
         omega_tc = 0
-    else:
+    elif P_t > 1:
         omega_tt = omega_t / P_t
         omega_tc = (1 - 1 / P_t) * omega_t
 
     return omega_cc, omega_ct, omega_tt, omega_tc
 
-# Colin/Emily output:
-shields_aa, f_wc, f_wt, ksdelta, ksw, fdelta, f_w, eta = vda.modelfunctions.combined_wavefric_ripples(T_cu, T_c, T_tu, T_t, a_hat, u_hat,
-                                                                              u_delta, delta, d50, d90, s, g)
-# Maggie output:
-fwdelt_c, fwdelt_t, TwRe, theta_cmag, theta_tmag, theta_cx, theta_tx = vda.modelfunctions.currentfric(u_delta, u_hat, f_wc, f_wt, rho, f_w,
-                                                                                   c_w, u_crx, u_trx, s, g, d50, fdelta)
-
-# Carly output:
-dstar      = vda.modelfunctions.dimensionless_grainsize(d50)
-shields_cr = vda.modelfunctions.critical_shields(dstar)
-omega_c    = vda.modelfunctions.sandload_crest(theta_cx, shields_cr)
-omega_t    = vda.modelfunctions.sandload_trough(theta_tx, shields_cr)
-
-# Carson output:
-delta_sc = vda.modelfunctions.sfl_thickness(theta_cmag, d50)
-delta_st = vda.modelfunctions.sfl_thickness(theta_tmag, d50)
 
 #%% Outputs
 
-omega_cc, omega_ct, omega_tt, omega_tc = phaseLag(s, d50, eta, u_hat_c, u_hat_t, c_w, T_c, T_cu, delta_sc, T_t, T_tu, delta_st, omega_c, omega_t, alpha=8.2, xi=1.7)
+# Tell Carly to change these outputs as numpy arrays
+omega_c = np.array(omega_c)
+omega_t = np.array(omega_t)
+delta_sc = delta_sc[0]
+delta_st = delta_st[0]
+
+omega_cc = []
+omega_ct = []
+omega_tt = []
+omega_tc = []
+
+for i in range(len(eta)):
+    omega_cc_temp, omega_ct_temp, omega_tt_temp, omega_tc_temp = phaseLag(s, d50, eta[i], u_hat_c[i], u_hat_t[i], c_w[i], T_c[i], T_cu[i], delta_sc, T_t[i], T_tu[i], delta_st, omega_c[i], omega_t[i], alpha=8.2, xi=1.7)
+
+    omega_cc.append(omega_cc_temp)
+    omega_ct.append(omega_ct_temp)
+    omega_tt.append(omega_tt_temp)
+    omega_tc.append(omega_tc_temp)
+
+omega_cc = np.array(omega_cc)
+omega_ct = np.array(omega_ct)
+omega_tt = np.array(omega_tt)
+omega_tc = np.array(omega_tc)
